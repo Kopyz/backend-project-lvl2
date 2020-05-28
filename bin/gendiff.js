3e25ebc2ad41e515e4d6f403899f8d37913aaf97
Saveload <saveload65@gmail.com>
2020-05-28 10:50:43 +0300

    added .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,9 @@ program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action(function (filepath1, filepath2) {
+    file1 = filepath1;
+    file2 = filepath2;
+   })
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
