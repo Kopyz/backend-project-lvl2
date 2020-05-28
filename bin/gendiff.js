7a1b6711961d3f15ce29a9fa0a8063fd76a6beea
Saveload <saveload65@gmail.com>
2020-05-28 10:43:33 +0300

    added .arguments and option -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,6 @@ const  program = require('commander');
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
