8a49138f140765b5416b174f31f35d805e9d47fe
Saveload <saveload65@gmail.com>
2020-06-03 11:22:01 +0300

    action section is changed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
+import compare from 'comparator.js';
 const  program = require('commander');
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .action(function (filepath1, filepath2) {
-    file1 = filepath1;
-    file2 = filepath2;
-   })
+  .action(compare(filepath1, filepath2))
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
