12b7df347883fafad7e0f592a697d02eb558f132
Saveload <saveload65@gmail.com>
2020-06-16 18:12:40 +0300

    add test for yaml

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -8,8 +8,14 @@ const expected1 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-1'), 'asci
 const expected2 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-2'), 'ascii'));
 const expected3 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-3'), 'ascii'));
 
-test('diff_1', () => {
+test('diff_JSON', () => {
   expect(compare(makePath('before.json'), makePath('afterEmpty.json'))).toEqual(expected1);
   expect(compare(makePath('beforeEmpty.json'), makePath('after.json'))).toEqual(expected2);
   expect(compare(makePath('before.json'), makePath('after.json'))).toEqual(expected3);
+});
+
+test('diff_yaml', () => {
+  expect(compare(makePath('before.yaml'), makePath('empty.yaml'))).toEqual(expected1);
+  expect(compare(makePath('empty.yaml'), makePath('after.yaml'))).toEqual(expected2);
+  expect(compare(makePath('before.yaml'), makePath('after.yaml'))).toEqual(expected3);
 });
