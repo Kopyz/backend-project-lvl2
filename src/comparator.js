8cacb69baec11629c8e13262f3b99a79a5356820
Saveload <saveload65@gmail.com>
2020-07-15 16:44:44 +0300

    add universal diff maker

--- src/comparator.js
+++ src/comparator.js
@@ -25,39 +25,41 @@ const compare = (pathToFile1, pathToFile2) => {
 const checkKeyPresence = (tree, key) => Object.prototype.hasOwnProperty.call(tree, key);
 
 const generateRecursiveDiff = (tree1, tree2) => {
-  const keys1 = Object.keys(tree1);
-  const keys2 = Object.keys(tree2);
+  const file1 = parse(tree1);
+  const file2 = parse(tree2);
+  const keys1 = Object.keys(file1);
+  const keys2 = Object.keys(file2);
   const allKeys = _.union(keys1, keys2);
 
   return allKeys.reduce((acc, key) => {
-    const value1 = tree1[key];
-    const value2 = tree2[key];
+    const value1 = file1[key];
+    const value2 = file2[key];
 
-    if (_.isObject(value1) && checkKeyPresence(tree1, key)
-      && checkKeyPresence(tree2, key) && !_.isObject(value2)) {
+    if (_.isObject(value1) && checkKeyPresence(file1, key)
+      && checkKeyPresence(file2, key) && !_.isObject(value2)) {
       acc[`- ${key}`] = generateRecursiveDiff(value1, value1);
       acc[`+ ${key}`] = value2;
       return acc;
     }
-    if (_.isObject(value1) && checkKeyPresence(tree1, key)
-      && checkKeyPresence(tree2, key)) {
+    if (_.isObject(value1) && checkKeyPresence(file1, key)
+      && checkKeyPresence(file2, key)) {
       acc[`  ${key}`] = generateRecursiveDiff(value1, value2);
       return acc;
     }
-    if (_.isObject(value1) && checkKeyPresence(tree1, key)
-      && !checkKeyPresence(tree2, key)) {
+    if (_.isObject(value1) && checkKeyPresence(file1, key)
+      && !checkKeyPresence(file2, key)) {
       acc[`- ${key}`] = generateRecursiveDiff(value1, value1);
       return acc;
     }
-    if (_.isObject(value2) && !checkKeyPresence(tree1, key) && checkKeyPresence(tree2, key)) {
+    if (_.isObject(value2) && !checkKeyPresence(file1, key) && checkKeyPresence(file2, key)) {
       acc[`+ ${key}`] = generateRecursiveDiff(value2, value2);
       return acc;
     }
     if (value2 === value1) {
       acc[`  ${key}`] = value1;
-    } else if (!checkKeyPresence(tree1, key)) {
+    } else if (!checkKeyPresence(file1, key)) {
       acc[`+ ${key}`] = value2;
-    } else if (!checkKeyPresence(tree2, key)) {
+    } else if (!checkKeyPresence(file2, key)) {
       acc[`- ${key}`] = value1;
     } else if (value2 !== value1) {
       acc[`+ ${key}`] = value2;
