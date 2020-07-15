e386097b352bfcd97d0bc127463fb50058285e1c
Saveload <saveload65@gmail.com>
2020-07-15 14:15:35 +0300

    add recursive logic

--- src/comparator.js
+++ src/comparator.js
@@ -22,4 +22,49 @@ const compare = (pathToFile1, pathToFile2) => {
   return resultData;
 };
 
-export default compare;
+const checkKeyPresence = (tree, key) => Object.prototype.hasOwnProperty.call(tree, key);
+
+const generateRecursiveDiff = (tree1, tree2) => {
+  const keys1 = Object.keys(tree1);
+  const keys2 = Object.keys(tree2);
+  const allKeys = _.union(keys1, keys2);
+
+  return allKeys.reduce((acc, key) => {
+    const value1 = tree1[key];
+    const value2 = tree2[key];
+
+    if (_.isObject(value1) && checkKeyPresence(tree1, key)
+      && checkKeyPresence(tree2, key) && !_.isObject(value2)) {
+      acc[`- ${key}`] = generateRecursiveDiff(value1, value1);
+      acc[`+ ${key}`] = value2;
+      return acc;
+    }
+    if (_.isObject(value1) && checkKeyPresence(tree1, key)
+      && checkKeyPresence(tree2, key)) {
+      acc[`  ${key}`] = generateRecursiveDiff(value1, value2);
+      return acc;
+    }
+    if (_.isObject(value1) && checkKeyPresence(tree1, key)
+      && !checkKeyPresence(tree2, key)) {
+      acc[`- ${key}`] = generateRecursiveDiff(value1, value1);
+      return acc;
+    }
+    if (_.isObject(value2) && !checkKeyPresence(tree1, key) && checkKeyPresence(tree2, key)) {
+      acc[`+ ${key}`] = generateRecursiveDiff(value2, value2);
+      return acc;
+    }
+    if (value2 === value1) {
+      acc[`  ${key}`] = value1;
+    } else if (!checkKeyPresence(tree1, key)) {
+      acc[`+ ${key}`] = value2;
+    } else if (!checkKeyPresence(tree2, key)) {
+      acc[`- ${key}`] = value1;
+    } else if (value2 !== value1) {
+      acc[`+ ${key}`] = value2;
+      acc[`- ${key}`] = value1;
+    }
+    return acc;
+  }, {});
+};
+
+export { compare, generateRecursiveDiff };
