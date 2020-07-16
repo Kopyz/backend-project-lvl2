ca4d1b684cef28fe3129ab752157c43b18e6812a
Saveload <saveload65@gmail.com>
2020-07-16 12:53:23 +0300

    test updated

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -7,12 +7,13 @@ const fs = require('fs');
 const expected1 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-1'), 'ascii'));
 const expected2 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-2'), 'ascii'));
 const expected3 = JSON.parse(fs.readFileSync(makePath('jsonTestSample-3'), 'ascii'));
+const expected4 = JSON.parse(fs.readFileSync(makePath('jsonTreeTest'), 'ascii'));
 
 test('diff_JSON', () => {
   expect(generateRecursiveDiff(makePath('before.json'), makePath('afterEmpty.json'))).toEqual(expected1);
   expect(generateRecursiveDiff(makePath('beforeEmpty.json'), makePath('after.json'))).toEqual(expected2);
   expect(generateRecursiveDiff(makePath('before.json'), makePath('after.json'))).toEqual(expected3);
-  expect(generateRecursiveDiff(makePath('before-tree.json'), makePath('after-tree.json'))).toEqual(expected3);
+  expect(generateRecursiveDiff(makePath('before-tree.json'), makePath('after-tree.json'))).toEqual(expected4);
 });
 
 test('diff_yaml', () => {
