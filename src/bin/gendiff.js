9cde307d9466aea9f4d68d68ab901fa1a142040b
Saveload <saveload65@gmail.com>
2020-07-29 17:25:56 +0300

    default

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ const program = require('commander');
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2> <format>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, format = program.format) => {
     const data = generateRecursiveDiff(filepath1, filepath2);
