9ea825b614243b40b7d833d43f8b650476e78c0b
Saveload <saveload65@gmail.com>
2020-07-29 16:24:18 +0300

    fix default format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2> <format>')
-  .action((filepath1, filepath2, format = program.option) => {
+  .action((filepath1, filepath2, format = program.format) => {
     const data = generateRecursiveDiff(filepath1, filepath2);
     printOutput(format)(data);
   })
