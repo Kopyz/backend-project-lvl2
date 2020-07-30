d89512c88eb1aa5ae4fead182b94ac4126fa3a00
Saveload <saveload65@gmail.com>
2020-07-30 15:52:34 +0300

    fix default format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,10 +7,10 @@ const program = require('commander');
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2> <format>')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2, format = program.format) => {
+  .action((filepath1, filepath2) => {
     const data = generateRecursiveDiff(filepath1, filepath2);
-    printOutput(format)(data);
+    printOutput(program.format)(data);
   })
   .parse(process.argv);
