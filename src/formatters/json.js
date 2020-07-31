6a5d687e29957b08416091c5708a98dc29d74436
Saveload <saveload65@gmail.com>
2020-07-31 18:01:38 +0300

    implement json format output

---
+++ src/formatters/json.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import generateRecursiveDiff from '../comparator';
+import getFixturePath from '../pathMaker';
+
+const fs = require('fs');
+
+const resultFilePath = getFixturePath('result');
+
+const removeExtraSpace = (tree) => {
+  const keys = Object.keys(tree);
+  return keys.reduce((acc, key) => {
+    if (_.isObject(tree[key])) {
+      acc[key.trim()] = removeExtraSpace(tree[key]);
+      return acc;
+    }
+    acc[key.trim()] = tree[key];
+    return acc;
+  }, {});
+};
+
+const makeJsonResult = (tree) => {
+  const diff = generateRecursiveDiff(tree);
+  const data = JSON.stringify(removeExtraSpace(diff));
+  fs.writeFileSync(resultFilePath, data);
+};
+
+export default makeJsonResult;
