dbec5b932847f33f47df3fad4b7853825dda3984
Saveload <saveload65@gmail.com>
2020-07-31 18:00:17 +0300

    add test for json format

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -2,6 +2,7 @@
 import { generateRecursiveDiff } from '../src/comparator';
 import stylish from '../src/formatters/stylish';
 import plain from '../src/formatters/plain';
+import json from '../src/formatters/json';
 import makePath from '../src/pathMaker';
 
 const fs = require('fs');
@@ -11,6 +12,7 @@ const expected2 = fs.readFileSync(makePath('testSample-2'), 'ascii');
 const expected3 = fs.readFileSync(makePath('testSample-3'), 'ascii');
 const expectedStylish = fs.readFileSync(makePath('testTreeSample'), 'ascii');
 const expectedPlain = fs.readFileSync(makePath('testPlainFormat'), 'ascii');
+const expectedJSON = fs.readFileSync(makePath('testJsonoutput'), 'ascii');
 
 const readResultFile = () => fs.readFileSync(makePath('result'), 'ascii');
 
@@ -53,4 +55,9 @@ test('diff_ini', () => {
 test('plain_diff', () => {
   plain(generateRecursiveDiff(makePath('before-tree.json'), makePath('after-tree.json')));
   expect(readResultFile()).toEqual(expectedPlain);
+});
+
+test('JSON output', () => {
+  json(generateRecursiveDiff(makePath('before-tree.json'), makePath('after-tree.json')));
+  expect(readResultFile()).toEqual(expectedJSON);
 });
