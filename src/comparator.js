2d6c9810f59f82e092861cb30e06311ea4d2c09e
Saveload <saveload65@gmail.com>
2020-08-13 16:45:12 +0300

    rebuild generate diff logic

--- src/comparator.js
+++ src/comparator.js
@@ -1,79 +1,36 @@
 import _ from 'lodash';
 import parse from './parsers';
 
-const checkKeyPresence = (tree, key) => Object.prototype.hasOwnProperty.call(tree, key);
+const generateRecursiveDiff = (filePath1, filePath2) => {
+  const data1 = parse(filePath1);
+  const data2 = parse(filePath2);
 
-const generateRecursiveDiff = (tree1, tree2) => {
-  const data1 = parse(tree1);
-  const data2 = parse(tree2);
-
-  const iter = (file1, file2) => {
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
+  const iter = (parsedData1, parsedData2) => {
+    const keys1 = Object.keys(parsedData1);
+    const keys2 = Object.keys(parsedData2);
     const allKeys = _.union(keys1, keys2);
 
     return allKeys.reduce((acc, key) => {
-      const value1 = file1[key];
-      const value2 = file2[key];
       const currentNode = {};
       currentNode.name = key;
 
-      if (!_.isObject(value1) && checkKeyPresence(file1, key)
-        && checkKeyPresence(file2, key) && _.isObject(value2)) {
-        currentNode.type = 'update';
-        currentNode['current value'] = iter(value2, value2);
-        currentNode['previous value'] = value1;
-        acc.push(currentNode);
-        return acc;
-      }
-
-      if (_.isObject(value1) && checkKeyPresence(file1, key)
-        && checkKeyPresence(file2, key) && !_.isObject(value2)) {
-        currentNode.type = 'update';
-        currentNode['current value'] = value2;
-        currentNode['previous value'] = iter(value1, value1);
-        acc.push(currentNode);
-        return acc;
-      }
-
-      if (_.isObject(value1) && checkKeyPresence(file1, key)
-        && checkKeyPresence(file2, key)) {
-        currentNode.type = 'unchange';
-        currentNode['current value'] = iter(value1, value2);
-        acc.push(currentNode);
-        return acc;
-      }
-
-      if (_.isObject(value1) && checkKeyPresence(file1, key)
-        && !checkKeyPresence(file2, key)) {
+      if (_.has(parsedData1, key) && !_.has(parsedData2, key)) {
         currentNode.type = 'remove';
-        currentNode['previous value'] = iter(value1, value1);
-        acc.push(currentNode);
-        return acc;
-      }
-
-      if (_.isObject(value2) && !checkKeyPresence(file1, key) && checkKeyPresence(file2, key)) {
+        currentNode.valueBefore = parsedData1[key];
+      } else if (!_.has(parsedData1, key) && _.has(parsedData2, key)) {
         currentNode.type = 'add';
-        currentNode['current value'] = iter(value2, value2);
-        acc.push(currentNode);
-        return acc;
-      }
-
-      if (value2 === value1) {
+        currentNode.valueAfter = parsedData2[key];
+      } else if (_.isObject(parsedData1[key]) && _.isObject(parsedData2[key])) {
+        currentNode.type = 'branch';
+        currentNode.children = iter(parsedData1[key], parsedData2[key]);
+      } else if (parsedData1[key] === parsedData2[key]) {
         currentNode.type = 'unchange';
-        currentNode['current value'] = value1;
-      } else if (!checkKeyPresence(file1, key)) {
-        currentNode.type = 'add';
-        currentNode['current value'] = value2;
-      } else if (!checkKeyPresence(file2, key)) {
-        currentNode.type = 'remove';
-        currentNode['previous value'] = value1;
-      } else if (value2 !== value1) {
+        currentNode.valueAfter = parsedData2[key];
+      } else {
         currentNode.type = 'update';
-        currentNode['current value'] = value2;
-        currentNode['previous value'] = value1;
+        currentNode.valueAfter = parsedData2[key];
+        currentNode.valueBefore = parsedData1[key];
       }
-
       acc.push(currentNode);
       return acc;
     }, []);
