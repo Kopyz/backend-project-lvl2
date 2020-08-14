362c13bbc54be3799270868654daaabf1b1e2109
Saveload <saveload65@gmail.com>
2020-08-14 16:43:11 +0300

    fix __dirname error

--- src/pathMaker.js
+++ src/pathMaker.js
@@ -1,5 +1,9 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const getFixturePath = (filename) => path.join(path.resolve(), '..', '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 export default getFixturePath;
